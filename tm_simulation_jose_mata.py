88e777cedc5d3f7032fc66042dee7c7d0c140ae0
José Mata <josemata@Joses-MacBook-Pro.local>
2024-12-11 11:47:11 -0500

    implemented printing the tape, the head, and the step-through feature

--- tm_simulation_jose_mata.py
+++ tm_simulation_jose_mata.py
@@ -11,18 +11,27 @@ class Turing_machine:
         self.states = [self.initial_state, 'q2', self.accept_state, self.reject_state]
         self.input_alphabet = ['_', '1', '0'] #The '_' represents a blank character 
         self.tape_alphabet = ['_', '1', '0', 'H'] #The 'H' means that the program has halted, so the H is at the end of the output on the tape
-        
+        self.current_state = 'q1' #This attribute can change
+        self.next_state = 'q1' #This attribute can change
 
-    def transition_function(self, input, current_state):
-        if input == 1 or input == 0:
-            self.write_value = 0
+    def transition_function(self, current_state, input):
+        if input == '1' or input == '0':
+            self.write_value = '0'
             self.direction_to_move = 'R'
-        if current_state == 'q1':
-            self.next_state = 'q2'
-        else:
-            self.next_state = 'q2'
-    def print_transition_function(self):
-        return "Write value: {}\nDirection to move: {}\nNext state: {}\n".format(self.write_value, self.direction_to_move, self.next_state)        
+            if current_state == 'q1':
+                self.next_state = 'q2'
+            elif current_state == 'q2':
+                self.next_state = 'q1'
+        elif input == '_':
+            self.write_value = '_'
+            self.direction_to_move = 'S'
+            self.next_state = self.accept_state
+
+        return (self.next_state, self.write_value, self.direction_to_move)
+    
+    def print_transition_function(self, current_state, input):
+        state = "δ({}, {}) = ({}, {}, {})\n".format(current_state, input, self.next_state, self.write_value, self.direction_to_move)  
+        return state  
 
 
 #The tape will simply be a list. It can be "infinite" since lists in Python are dynamic and can be made larger or smaller as needed.    
@@ -45,6 +54,13 @@ def load_tape(input_string):
         tape.append(character)
     return tape
 
+#The "print_tape" function converts the tape list into a string with no spaces in between the characters
+def print_tape(tape):
+    current_tape = ""
+    for character  in tape:
+        current_tape = current_tape + character
+    return current_tape
+
 #tm1 is the instance of the Turing Machine used in this application
 tm1 = Turing_machine()
 
@@ -53,6 +69,31 @@ input_valid = check_input_string(input_string, tm1.input_alphabet) #sends the in
 if input_valid == True:
     tape = load_tape(input_string)
     #print("The contents of the tape are: " + str(tape))
+    index = 0
+    step = 0
+    while tm1.current_state != tm1.accept_state:
+        tm1.next_state, tm1.write_value, tm1.direction_to_move =tm1.transition_function(tm1.current_state, tape[index]) #carry out transition function 
+        step = step + 1
+        #print info
+        print("Step: " + str(step) + "\n")
+        print("State: " + tm1.current_state + "\n")
+        current_tape = print_tape(tape)
+        print("Tape: " + current_tape + "\n")
+        number_of_spaces = " " * index
+        print("Head: " + number_of_spaces + "^\n")
+        print("Next: " + tm1.print_transition_function(tm1.current_state, tape[index]) + "\n\n")
+        #write a '0' unless it is the last character (blank)
+        if tm1.write_value == '0': 
+            tape[index] = '0'
+        elif tm1.write_value == '_':
+            tape[index] = '_'
+        #move to the right unless it is the last character 
+        if tm1.direction_to_move == 'R':
+            index = index + 1
+        #the current state becomes the next state for the while loop to begin again
+        tm1.current_state = tm1.next_state
+        enter = input("Press 'Enter' to continue.")
+    print("The output tape is: " + current_tape)
 else:
     print("Tape CANNOT contain a character not from the input alphabet.\n")
 
